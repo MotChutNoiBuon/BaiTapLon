badafb090d4d3e63e730ea1395246b2a6ece1c11
Loc <locn190904@gmail.com>
2023-08-08 16:10:00 +0700

    sơ sơ trang kho sách
    Co-Authored-By: Nvgb1608 <140680485+Nvgb1608@users.noreply.github.com>

---
+++ js/backtotop.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop()) {
+            $('#myBtn').fadeIn();
+        } else
+        $('#myBtn').fadeOut();
+    });
+    $("#myBtn").click(function(){
+        $('html, body').animate({scrollTop: 0
+        },1000);
+    });
+
+    $(window).load(function() {
+        if ($('header').length > 0) {
+         var _top = $('header').offset().top - parseFloat($('header').css('marginTop').replace(/auto/, 0));
+         $(window).scroll(function(evt) {
+          var _y = $(this).scrollTop();
+          if (_y > _top) {
+           $('header').addClass('fixed');
+           $('.main-1').css("margin-top", "30px")
+          } else {
+           $('header').removeClass('fixed');
+           $('.main-1').css("margin-top", "0")
+          }
+         })
+        }
+       });
+});
