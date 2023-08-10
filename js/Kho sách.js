b62199af3d5e1308987f48a078501597a0785aed
Loc <locn190904@gmail.com>
2023-08-10 15:00:02 +0700

    tạm tạm
    Co-Authored-By: Nvgb1608 <140680485+Nvgb1608@users.noreply.github.com>

--- js/Kho sách.js
+++ js/Kho sách.js
@@ -1,11 +1,16 @@
-function loadProducts() {
+function loadBooks() {
     fetch("data/books.json").then(res => res.json()).then(data => {
+        
         let h = "";
+        let m = "";
+
+
         for (let p of data) {
             h += `
+
             <div class="book">
                 <div>
-                    <div><img src="${p.image}" alt="iPhone" /></div>
+                    <div><img src="${p.image}" alt="book" /></div>
                     <h3>${p.name}</h3>
                     <p>${p.price} VNĐ</p>
                     
@@ -13,8 +18,52 @@ function loadProducts() {
             </div>
             `
         }
+        m=`<h3>Mưa</h3>`;
+        let y = document.querySelector(".books1");
+        y.insertAdjacentHTML("beforebegin", m);
+
+        
+        let e = document.getElementById("books1");
+        e.innerHTML += h;
+        
+        
+
     })
 }
+function loadBooks2() {
+    fetch("data/books2.json").then(res => res.json()).then(data => {
+        
+        let h = "";
+        let m = "";
+
+
+        for (let p of data) {
+            h += `
+
+            <div class="book">
+                <div>
+                    <div><img src="${p.image}" alt="book" /></div>
+                    <h3>${p.name}</h3>
+                    <p>${p.price} VNĐ</p>
+                    
+                </div>
+            </div>
+            `
+        }
+        m=`<h3>Nắng</h3>`;
+        let y = document.querySelector(".books2");
+        y.insertAdjacentHTML("beforebegin", m);
+
+        
+        let e = document.getElementById("books2");
+        e.innerHTML += h;
+        
+        
+
+    })
+}
+
 window.onload = () => {
-    loadProducts();
+    loadBooks();
+    loadBooks2();
 }
