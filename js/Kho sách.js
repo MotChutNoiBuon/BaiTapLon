19c686f341ecb32677c40137c64f0638636a01c4
Loc <locn190904@gmail.com>
2023-08-12 16:34:47 +0700

    chỉnh lại xí về màu
    Co-Authored-By: Nvgb1608 <140680485+Nvgb1608@users.noreply.github.com>

--- js/Kho sách.js
+++ js/Kho sách.js
@@ -10,8 +10,9 @@ function loadBooks() {
 
             <div class="book">
                 <div>
-                    <div><img src="${p.image}" alt="book" /></div>
-                    <h3>${p.name}</h3>
+                    <div> <a href="${p.link}"> <img src="${p.image}" alt="book" /></a>
+                    </div>
+                    <a href="${p.link}"> <h3>${p.name}</h3></a>
                     <p>${p.price} VNĐ</p>
                     
                 </div>
@@ -42,15 +43,17 @@ function loadBooks2() {
 
             <div class="book">
                 <div>
-                    <div><img src="${p.image}" alt="book" /></div>
-                    <h3>${p.name}</h3>
+                    <div> <a href="${p.link}"> <img src="${p.image}" alt="book" /></a>
+                    </div>
+                    <a href="${p.link}"> <h3>${p.name}</h3></a>
                     <p>${p.price} VNĐ</p>
-                    
+
+                
                 </div>
             </div>
             `
         }
-        m=`<h3>Nắng</h3>`;
+        m=`<h3>Mưa 2</h3>`;
         let y = document.querySelector(".books2");
         y.insertAdjacentHTML("beforebegin", m);
 
@@ -62,8 +65,112 @@ function loadBooks2() {
 
     })
 }
+function loadBooks3() {
+    fetch("data/books3.json").then(res => res.json()).then(data => {
+        
+        let h = "";
+        let m = "";
+
+
+        for (let p of data) {
+            h += `
+
+            <div class="book">
+                <div>
+                    <div> <a href="${p.link}"> <img src="${p.image}" alt="book" /></a>
+                    </div>
+                    <a href="${p.link}"> <h3>${p.name}</h3></a>
+                    <p>${p.price} VNĐ</p>
+                
+                </div>
+            </div>
+            `
+        }
+        m=`<h3>Mưa 3</h3>`;
+        let y = document.querySelector(".books3");
+        y.insertAdjacentHTML("beforebegin", m);
+
+        
+        let e = document.getElementById("books3");
+        e.innerHTML += h;
+        
+        
+
+    })
+}
+function loadBooks4() {
+    fetch("data/books4.json").then(res => res.json()).then(data => {
+        
+        let h = "";
+        let m = "";
+
+
+        for (let p of data) {
+            h += `
+
+            <div class="book">
+                <div>
+                    <div> <a href="${p.link}"> <img src="${p.image}" alt="book" /></a>
+                    </div>
+                    <a href="${p.link}"> <h3>${p.name}</h3></a>
+                    <p>${p.price} VNĐ</p>
+                
+                </div>
+            </div>
+            `
+        }
+        m=`<h3>Mưa 4</h3>`;
+        let y = document.querySelector(".books4");
+        y.insertAdjacentHTML("beforebegin", m);
+
+        
+        let e = document.getElementById("books4");
+        e.innerHTML += h;
+        
+        
+
+    })
+}
+function loadBooks5() {
+    fetch("data/books5.json").then(res => res.json()).then(data => {
+        
+        let h = "";
+        let m = "";
+
+
+        for (let p of data) {
+            h += `
+
+            <div class="book">
+                <div>
+                    <div> <a href="${p.link}"> <img src="${p.image}" alt="book" /></a>
+                    </div>
+                    <a href="${p.link}"> <h3>${p.name}</h3></a>
+                    <p>${p.price} VNĐ</p>
+
+                    
+                </div>
+            </div>
+            `
+        }
+        m=`<h3>Mưa 5</h3>`;
+        let y = document.querySelector(".books5");
+        y.insertAdjacentHTML("beforebegin", m);
+
+        
+        let e = document.getElementById("books5");
+        e.innerHTML += h;
+        
+        
+
+    })
+}
 
 window.onload = () => {
     loadBooks();
     loadBooks2();
+    loadBooks3();
+    loadBooks4();
+    loadBooks5();
+
 }
